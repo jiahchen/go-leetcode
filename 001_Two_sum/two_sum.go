ee8891f5a58858155a866c79fd0fdc5ae741bb48
jiahchen <jianhaoch@gmail.com>
2019-09-01 01:44:47 +0800

    add 1 two sum solution

--- 001_Two_sum/two_sum.go
+++ 001_Two_sum/two_sum.go
@@ -3,18 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	input := []int{2, 7, 11, 5}
-	target := 9
+	input := []int{1, 3, 5, 7}
+	target := 12
 	fmt.Println("Ans: ", twoSum(input, target))
 }
 
 func twoSum(nums []int, target int) []int {
+	match := make(map[int]int)
 	for i := 0; i < len(nums); i++ {
-		for j := i + 1; j < len(nums); j++ {
-			if nums[i]+nums[j] == target {
-				return setAns(i, j)
-			}
+		var tmp = target - nums[i]
+		if match[nums[i]] > 0 {
+			return setAns(match[nums[i]]-1, i)
 		}
+		match[tmp] = i + 1
 	}
 	return setAns(0, 0)
 }
